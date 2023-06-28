b60ce0ec7b6a1505fd059127bcbbf25b177808c8
carolina <carolinacapetilloc@gmail.com>
2023-06-28 15:19:14 -0700

    solved first problem

--- arrayPrototypeLast.js
+++ arrayPrototypeLast.js
@@ -1,11 +1,10 @@
 // daily coding challenges 
 // ARRAY PROTOTYPE LAST
 
-// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. 
+// If there are no elements in the array, it should return -1.
 
 // You may assume the array is the output of JSON.parse.
-
- 
 
 // Example 1:
 
@@ -22,3 +21,12 @@
 // Constraints:
 
 // 0 <= arr.length <= 1000
+
+
+Array.prototype.last = function() {
+    if (this.length === 0) {
+        return -1
+    } else {
+        return this[this.length-1];
+    };
+}
